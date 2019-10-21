3fd66338db6f537197f279e5d9edd33c0f3df420
Anastasiia <nastia4019@gmail.com>
2019-10-21 23:27:56 +0300

    first-commit

---
+++ src/js/index.js
@@ -0,0 +1,107 @@
+
+$(document).ready(function () {
+    $('body').on('click', '.header__button-close', function () {
+        $(this).closest('.header').hide();
+    });
+    PopUpHide();
+    PopUpHide2();
+});
+$('.name').keyup(function () {
+    this.value = this.value.replace(/[^а-я0-9\.]/g, '');
+});
+$('.phone').keyup(function () {
+    this.value = this.value.replace(/[^0-9\.]/g, '');
+});
+$(function () {
+
+    var addInput = '#input1';
+    var n = 1;
+    $(addInput).val(n);
+    $('.main-block__plus').on('click', function () {
+        $(addInput).val(++n);
+    })
+    $('.main-block__minus').on('click', function () {
+        if (n >= 1) {
+            $(addInput).val(--n);
+        } else {
+        }
+    });
+});
+
+function PopUpShow() {
+    $(".b-popup").show()
+}
+function PopUpHide() {
+    $(".b-popup").hide();
+}
+
+$('.b-popup__email').on('blur', function () {
+    let email = $(this).val();
+    if (email.length > 0
+        && (email.match(/.+?\@.+/g) || []).length !== 1) {
+        console.log('invalid');
+    } else {
+        console.log('valid');
+    }
+});
+
+(function () {
+    $('.phone').attr("placeholder", $('.b-popup__select').val())
+    $('.b-popup__select').on('change', function () {
+        $('.phone').attr("placeholder", $('.b-popup__select').val())
+    })
+})()
+
+$(document).mouseup(function (e) {
+    var container = $(".b-popup");
+    if (container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+function PopUpShow2() {
+    $(".b-popup2").show()
+    $(".b-popup").hide();
+}
+function PopUpHide2() {
+    $(".b-popup2").hide();
+}
+
+$('form').submit(function () {
+
+    var name = $(".name").val();
+    if (name === '') {
+
+        return false;
+    }
+    else { PopUpShow2(); }
+
+
+});
+
+function updatePrice() {
+    var price = $("#input1").val();
+    var total = (parseFloat(price) + 1) * 10.99;
+    console.log(total);
+    $("#input2").empty()
+    $('#input2').append(total);
+};
+
+
+
+
+
+function formatState(state) {
+    if (!state.id) {
+        return state.text;
+    }
+    var baseUrl = "../images";
+    var $state = $(
+        '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
+    );
+    return $state;
+};
+
+$(".js-example-templating").select2({
+    templateResult: formatState
+});
